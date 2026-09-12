e4d411a2f4824e0f3eea10edcf853eb8de0c9903
Znerzi <diazrenz18@gmail.com>
2026-09-12 08:39:37 +0800

    app.component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,4 +2,8 @@ import { TestBed } from '@angular/core/testing';
 import { provideRouter } from '@angular/router';
 import { AppComponent } from './app.component';
 
-     providers: [provideRouter([])]
+describe('AppComponent', () => {
+  it('should create the app', async () => {
+    await TestBed.configureTestingModule({
+      imports: [AppComponent],
+      providers: [provideRouter([])]
