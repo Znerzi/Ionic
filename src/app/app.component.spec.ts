b503078228a112e0ca35b548baa55e9eb6cdbfd1
Znerzi <diazrenz18@gmail.com>
2026-09-19 08:18:32 +0800

    app.component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,2 +1,5 @@
 import { TestBed } from '@angular/core/testing';
 import { provideRouter } from '@angular/router';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
