f83ca0228a29d2082fdcca73a704dc01a5a68b57
Znerzi <diazrenz18@gmail.com>
2026-09-20 09:24:01 +0800

    app.route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,4 +5,7 @@ export const routes: Routes = [
     path: 'home',
     loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
   },
-
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMat
